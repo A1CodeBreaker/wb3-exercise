cc14ec72a78280ba6676e4ff6fca937cd0925679
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-05-01 10:55:39 -0400

    added a script that makes and calls basic functions.

---
+++ simple_functions.js
@@ -0,0 +1,23 @@
+function favoriteThings() {
+    console.log("Name: Arondre Shelton");
+    console.log("Favorite Movie: Avengers: Endgame");
+    console.log("Favorite band: The Beatles");
+  }
+  
+  function whyImHere() {
+    console.log(
+      "I am here to improve my programing skills and learn more about the software development process from a real company. Getting a job would be nice too."
+    );
+  }
+  
+  function favortiePlace() {
+    console.log(
+      "Favorite Place: My favorite place to travel is Miami beacuse of its beautiful beaches,great weather,history,culture,sports and entertainment.  "
+    );
+  }
+  
+  favoriteThings();
+  
+  whyImHere();
+  
+  favortiePlace();
