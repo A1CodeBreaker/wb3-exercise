fed43be51b3ab017d411323135e53a9d77e7f365
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-05-02 19:30:37 -0400

    date formats completed.

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,7 @@
+const myBirthday = new Date('1984-04-18T12:00:00');
+const momsBirthday = new Date('1974-04-18T12:00:00');
+const dadsBirthday = new Date('1975-12-28T12:00:00');
+
+console.log(myBirthday.toLocaleString());
+console.log(momsBirthday.toLocaleString());
+console.log(dadsBirthday.toLocaleString());
